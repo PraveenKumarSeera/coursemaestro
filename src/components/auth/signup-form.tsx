16e3c67c7d6d84739e4f3fdc3da1743086fcac6d
xx_Wanted_420_xx <xx420wantedxx@gmail.com>
2025-10-07 03:17:28 +0000

    TypeError: (0 , __TURBOPACK__imported__module__$5b$project$5d2f$node_mod

--- src/components/auth/signup-form.tsx
+++ src/components/auth/signup-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { signup } from '@/app/actions/auth';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
